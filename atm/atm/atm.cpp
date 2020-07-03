496e7ae58e82d844ac4e2d28aca400a80a0dfb6a
twawaa <tokura_0521@outlook.jp>
2020-07-03 19:55:06 +0900

    ヘッダファイルコミット

--- atm/atm/atm.cpp
+++ atm/atm/atm.cpp
@@ -1,9 +1,30 @@
-﻿#include <stdio.h>
+﻿/******************************************************/
+/* ATMシステムメインプログラム(入力と出力)            */
+/******************************************************/
 
-int main()
+#include "atm.h"
+#include <stdio.h>
+
+/* カレントディレクトリ取得*/
+// 戻り値：なし
+void getGurrentDirectory(char* currentDirectory)
 {
-    FILE* fp;//ファイルポインタ
+	GetCurrentDirectory(CHARBUFF, currentDirectory);
+}
+
+
+
+/*メインプログラム*/
+int main()
+{   
+    //ファイルポインタ
+    FILE* fp;
     errno_t error;
+
+    //ユーザ情報
+    USER user;
+    //口座残高保存用
+    char buf[CHARBUFF] = {0};
 
     error = fopen_s(&fp, "Results.txt", "w");
     if (error != 0) {
@@ -11,4 +32,27 @@ int main()
         fprintf_s(stderr, "failed to open");
         return -1;
     }
+
+	//カレントディレクトリ
+	char currentDirectory[CHARBUFF];
+	getGurrentDirectory(currentDirectory);
+
+	// iniファイルから読み込み
+	
+	
+	// 暗証番号の読み込み
+	
+	// 口座残高の読み込み
+	
+	// 口座残高をiniファイルに書き込み
+	
+
+	// 口座残高をresult.txtに書き込み
+	
+	// ファイルクローズ
+	fclose(fp);
+	return 0;
+
+
+
 }
