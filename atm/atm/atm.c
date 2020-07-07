738f2dba85b294145006d0a1ce331144395fef15
twawaa <tokura_0521@outlook.jp>
2020-07-07 22:50:32 +0900

    　Update atm.c

--- atm/atm/atm.c
+++ atm/atm/atm.c
@@ -44,16 +44,16 @@ int main()
 	sprintf_s(settingFile, CHARBUFF, "%setting.ini", currentDirectory);
 
 	//暗証番号の読み込み
-	if (GetPrivateProfilrString(section, keyWord, "none", user.PIN, CHARBUFF, settingFile) == 0) {
+	if (GetPrivateProfileString(section, keyWord, "none", user.PIN, CHARBUFF, settingFile) == 0) {
 		//読み込みエラー
 		fprintf_s(stderr, "fileld to read PIN");
 		return -1;
 	}
 	// 口座残高の読み込み
 	sprintf_s(keyWord, CHARBUFF, "Money");
-	if (GetPrivateProfilesString(section, keyWord, "none", buf, CHARBUFF, settingFile) == 0) {
+	if (GetPrivateProfileString(section, keyWord, "none", buf, CHARBUFF, settingFile) == 0) {
 		//読み込みエラー
-		fprintf_s(stderr, "fileld to read Money")
+		fprintf_s(stderr, "fileld to read Money");
 		return -1;
 	}
 	user.Money = strtoull(buf, NULL, 10);
@@ -62,12 +62,12 @@ int main()
 	mainScreen(fp, &user);
 
 	// 口座残高をiniファイルに書き込み
-	sprintf_s(buf, ChARBUFF, "%llu".user.Money);
-	sprintf_s(keyWorf, CHARBUFF, "Money");
+	sprintf_s(buf, CHARBUFF, "%llu",user.Money);
+	sprintf_s(keyWord, CHARBUFF, "Money");
 	WritePrivateProfileString(section, keyWord, buf, settingFile);
 
 	// 口座残高をresult.txtに書き込み
-	sprint_s(fp, "end of transaction : money=%llu\n", user.Money);
+	fprintf_s(fp, "end of transaction : money=%llu\n", user.Money);
 	// ファイルクローズ
 	fclose(fp);
 	return 0;
